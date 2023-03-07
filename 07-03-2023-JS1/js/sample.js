6e6f102e128c824d3ff1ef5e51d7abd3b300fb5a
arunkumard2002 <arunkumar.d2002@gmail.com>
2023-03-07 09:08:57 +0530

    07-03-2023 javascript basics

---
+++ 07-03-2023-JS1/js/sample.js
@@ -0,0 +1,50 @@
+let student = 'arunkumar';
+let subject = 'javascript';
+debugger;
+console.log("hi",student, subject);
+student = 'xyz';
+console.log("hi",student, subject);
+const students = {
+    studentName: 'blah blash'
+};
+students.studentName = 'studentname' 
+console.log(students);
+
+// Primitive DataTypes
+// Number -- 
+// ,Boolean, String, Undefined, null
+
+// Non - Primitive  DataTypes
+// Arrays --- [] -
+// Object --- {}
+
+let number = 123;
+console.log(number);
+console.log(typeof number);
+
+let mystring = 'my string';
+console.log(mystring);
+console.log(typeof mystring);
+
+let un = null;
+console.log(un);
+console.log(typeof un);
+
+let myArray = []; // new Array(5)
+console.log(myArray);
+console.log(typeof myArray);
+console.log(Array.isArray(myArray));
+
+let myObject = {}; // new Object();
+console.log(myObject);
+console.log(typeof myObject);
+console.log(Array.isArray(myObject));
+
+let abc = 'abc';
+console.log(abc);
+
+abc = 123;
+console.log(typeof abc);
+
+abc = [];
+console.log(abc);
